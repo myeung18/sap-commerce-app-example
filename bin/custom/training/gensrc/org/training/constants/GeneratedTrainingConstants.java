e170d5c70d3ef69fb272a9038d96f2b087744d9d
xiezhang <xiezhang@redhat.com>
2020-08-26 16:08:53 -0400

    Create training extension

---
+++ bin/custom/training/gensrc/org/training/constants/GeneratedTrainingConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Aug 21, 2020, 4:03:16 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package org.training.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingConstants
+{
+	public static final String EXTENSIONNAME = "training";
+	
+	protected GeneratedTrainingConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
